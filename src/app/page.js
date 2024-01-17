e8fe4ac8832f86a3139a9a01df13dd1a0f39fe3f
KaungMyat <aungakm667@gmail.com>
2024-01-18 00:46:06 +0630

    fixed cache

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/app/config/firebase";
 
 import HomeElements from "./_reuse-components/HomeElements";
-
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export default async function Home() {
   const userCollection = collection(db, "medicine");
   const data = await getDocs(userCollection);
